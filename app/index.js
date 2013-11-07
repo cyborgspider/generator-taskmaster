d04a15fb1acffbe350d1a70441d850cac877f7df
Carlos Ortega <exoticorion@gmail.com>
2013-11-07 15:51:14 -0800

    initial commit

---
+++ app/index.js
@@ -0,0 +1,63 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var TaskmasterGenerator = module.exports = function TaskmasterGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(TaskmasterGenerator, yeoman.generators.Base);
+
+TaskmasterGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // have Yeoman greet the user.
+  console.log(this.yeoman);
+
+  var prompts = [
+  {
+    name:    'siteName',
+    message: 'What is the name of your site or project?',
+  },
+  {
+    name:    'authorName',
+    message: 'Who is the creator of this site?',
+    default: "Carlos Ortega"
+  }
+  ];
+
+  this.prompt(prompts, function (props) {
+    this.siteName   = props.siteName;
+    this.authorName = props.authorName;
+    cb();
+  }.bind(this));
+};
+
+TaskmasterGenerator.prototype.app = function app() {
+  this.mkdir('site');
+  this.mkdir('site/styles');
+  this.mkdir('site/scripts');
+  this.mkdir('site/images');
+
+  this.copy('_package.json', 'package.json');
+  this.copy('_bower.json', 'bower.json');
+  this.copy('_GruntFile.js', 'GruntFile.js');
+  this.copy('_gitignore', '.gitignore');
+
+  this.copy('_script.coffee', 'site/scripts/scripts.coffee');
+  this.copy('_style.styl', 'site/styles/styles.styl');
+  this.template('_index.jade', 'site/index.jade');
+};
+
+// TaskmasterGenerator.prototype.projectfiles = function projectfiles() {
+//   this.copy('editorconfig', '.editorconfig');
+//   this.copy('jshintrc', '.jshintrc');
+// };
